7da46bcc3a3c90ac70ed5159161e860ed22ecd49
yeahitsjoyce <51183131+yeahitsjoyce@users.noreply.github.com>
2022-12-02 20:25:06 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask
 from flask import request
 from db import db, Artist, Album, Song
 
+"""
+What we need:
+
+file.db
+controller.py
+"""
 app = Flask(__name__)
 db_filename = 'spotify.db'
 
@@ -14,6 +20,12 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code
+comment_id_counter = 1
 @app.route('/')
 def start():
     return 'Spotify Clone'
@@ -48,5 +60,78 @@ def delete_artist(artist_id):
         return json.dumps({"error": "Artist not found"}), 404
     DB.delete_artist_by_id(artist_id)
     return json.dumps({"id":artist.get("id"),"name":artist.get("name"),"username":artist.get("username")}), 200
+
+@app.route("/api/artists/albums/songs/<int:song_id>/comments/")
+def get_song_comments(song_id):
+    song = songs.get(song_id)
+    if song is None:
+        return json.dumps({"error": "Post not found!"}), 404
+
+    alist = []
+    for i in range(len(comments)):
+        if comments[i].get("song_id") == song_id:
+
+            id = comments[i].get("id")
+            likes = comments[i].get("upvotes")
+            text = comments[i].get("text")
+            username = comments[i].get("username")
+
+            alist.append({"id":id,
+            "likes":upvotes,
+            "text":text,
+            "username":username
+            })
+    res = {"comments": list(alist)}
+    return json.dumps(res), 200
+
+@app.route("/api/artists/albums/songs/<int:song_id>/comments/", methods=["POST"])
+def create_song_comment(song_id):
+    global comment_id_counter
+    body = json.loads(request.data)
+    text = body.get("text")
+    username = body.get("username")
+
+    comment = {"songid": song_id,
+    "id": comment_id_counter,
+    "like": 1,
+    "text": text,
+    "username": username
+    }
+    comments[comment_id_counter] = comment
+    comment_id_counter += 1
+    return json.dumps(comment),200
+
+@app.route("/api/artists/albums/songs/<int:song_id>/comments/<int:comment_id>/", methods=["POST"])
+def editComment(song_id,comment_id):
+    comment = comments.get(comment_id)
+    if comment is None:
+        return json.dumps({"error": "Comment not found"}), 404
+    body = json.loads(request.data)
+    text = body.get("text")
+    comment["text"] = text
+    return json.dumps(comment), 200
+
+# @app.route("/api/artists/<int:artist_id>/song/", methods=["POST"])
+# def create_assignment(artist_id):
+#     """
+#     Endpoint for creating a new assignment(song) for a course(artist) by id
+#     """
+#     song = Song.query.filter_by(id=song_id).first()
+#     if song is None:
+#         return failure_response("Song not found!")
+#     body = json.loads(request.data)
+#     if body.get("Name") is None or body.get("album_id") is None:
+#         return failure_response("Enter a title and due_date!",400)
+#     title = body.get("title")
+#     due_date = body.get("due_date")
+#     new_assignment = Assignment(title=title, due_date=due_date, song_id=song_id)
+#     db.session.add(new_assignment)
+#     course.assignments.append(new_assignment)
+#     db.session.commit()
+#     return success_response(new_assignment.serialize(), 201)
+#
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
