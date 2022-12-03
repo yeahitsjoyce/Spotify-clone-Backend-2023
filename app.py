ad36052860186aa25e4969b67d2c65d8e3e33585
yeahitsjoyce <51183131+yeahitsjoyce@users.noreply.github.com>
2022-12-02 20:30:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -133,5 +133,3 @@ def editComment(song_id,comment_id):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
